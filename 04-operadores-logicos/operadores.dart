6869074490156e4c1395e765114098f3335a166f
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-06-22 18:55:26 -0300

    Operadores

---
+++ 04-operadores-logicos/operadores.dart
@@ -0,0 +1,20 @@
+void main(){
+    double valor1 = 16.6;
+    int valor2 = 38;
+
+    double resultado = valor1/valor2;
+    print(resultado)
+
+    //OPERADORES
+    int operador1 = 1;
+    int operador2 = 2;
+
+    bool resultado = operador1 == operador2
+    bool resultado = operador1 > operador2
+    bool resultado = operador1 >= operador2
+    bool resultado = operador1 < operador2
+    bool resultado = operador1 <= operador2
+
+    print(1 is int)
+    print("palavra" is String)
+}
