dc74b99c76c1ba64c759dcb3cd17dca14ce86195
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-06-22 18:18:16 -0300

    Concatenação de variáveis

---
+++ 02-variaveis/concatenacao.dart
@@ -0,0 +1,7 @@
+void main(){
+    String produto = "Arroz Branco"
+    double preco = 10.99
+    print("O Arroz Branco esta 10.99")
+    print("O " + produto + " esta " + preco)
+    print("O $produto esta $preco")
+}
